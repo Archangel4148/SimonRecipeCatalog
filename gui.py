440c4532e549bd82b3ed273b62a57de3ccaa4c93
Joshua Santy <joshuasanty100@gmail.com>
2025-06-08 22:06:38 -0500

    added QTDesigner

--- gui.py
+++ gui.py
@@ -1,37 +1,33 @@
-#!/usr/bin/python
-
-"""
-ZetCode PyQt5 tutorial
-
-This example shows an icon
-in the titlebar of the window.
-
-Author: Jan Bodnar
-Website: zetcode.com
-"""
 import random
 import sys
+import time
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QPushButton, QDesktopWidget, QWidget, QLabel, \
-    QHBoxLayout, QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget
+
+from uic5 import Ui_Form
 
 
 class Example(QWidget):
 
     def __init__(self, data, nrows):
         super().__init__()
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
         self.nrows = nrows
         self.data = data
-        self.initUI()
+
+        # Button Connections
+        self.ui.generate_button.clicked.connect(self.getRandomRecipe)
+        self.ui.quit_button.clicked.connect(self.close)
 
     def getRandomRecipe(self):
-        randNum = random.randint(0, self.nrows)
+        randNum = random.randint(0, self.nrows - 1)
         data = self.data
-        # data["ingredients"] = data["ingredients"].apply(lambda x: '\n'.join(x) if isinstance(x, list) else str(x))
 
-        self.label.setText(f"""
+        print(randNum)
+        self.ui.recipe_label.setText(f"""
             <div>
             <b>{data["title"][randNum]}</b><br><br>
             <b>Ingredients: </b> \n {data["ingredients"][randNum]}<br><br>
@@ -41,90 +37,10 @@ class Example(QWidget):
             <b>Home Website: </b>{data["site"][randNum]}<br><br>
         </div> """)
 
-    def initUI(self):
-        # Layout:
-        hbox = QHBoxLayout()
 
-        self.label = QLabel("Recipe Time :D", self)
-        self.label.setWordWrap(True)
-        self.label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        # label.move(400, 400)
-
-        # Generate Button
-        gbtn = QPushButton('Get a Random Recipe!', self)
-        gbtn.clicked.connect(self.getRandomRecipe)
-
-        gbtn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        # gbtn.resize(1500, 150)
-        # gbtn.move(100, 100)
-        hbox.addWidget(gbtn)
-
-        # Quit Button:
-        qbtn = QPushButton('Quit', self)
-
-        qbtn.clicked.connect(QApplication.instance().quit)
-        qbtn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        # qbtn.resize(150,150)
-        # qbtn.move(50, 50)
-        hbox.addWidget(qbtn)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addWidget(self.label)
-
-        vbox.addStretch(1)
-
-        vbox.addLayout(hbox)
-
-        # Menu
-
-        # Menu Items:
-        # menubar = self.menuBar()
-        # fileMenu = menubar.addMenu('File')
-        #
-        # impMenu = QMenu('Test 1', self)
-        #
-        # #Qactions
-        #
-        # impAct=QAction('Test 2', self)
-        #
-        # #Add Action
-        #
-        # impMenu.addAction(impAct)
-        #
-        # newAct = QAction('New', self)
-        #
-        # fileMenu.addAction(newAct)
-        # fileMenu.addMenu(impMenu)
-
-        # # Status Bar
-        # statusbar = self.statusBar()
-        # statusbar.showMessage('Ready')
-
-        # Location and Size of window
-        # self.setGeometry(300, 300, 300, 220)
-
-        self.setLayout(vbox)
-
-        # Maximize Screen
-        self.showMaximized()
-        self.setWindowTitle('Santy\'s Recipe Catalog')
-        self.setWindowIcon(QIcon('chicken.JPG'))
-
-        self.show()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topleft())
-
-
-def main():
+def main(data, nrows):
     app = QApplication(sys.argv)
-    ex = Example()
+    window = Example(data, nrows)
+    window.show()
+
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 import ast
-import sys
 
 import pandas as pd
-import random
 
-from PyQt5.QtWidgets import QApplication
+from gui import main
 
-from gui import main, Example
-
-
-def main(data, nrows):
-    app = QApplication(sys.argv)
-    ex = Example(data, nrows)
-
-    # print(ex.label.text())
-
-    # ex.label.setText(recipe)
-
-    sys.exit(app.exec_())
-
-nrows = 20000
+nrows = 10
 data = pd.read_csv("recipe catalog raw/recipes_data.csv", nrows=nrows)
 data.drop(columns=["source"])
-#Stringified lists
 
+# Stringified lists
 data["ingredients"] = data["ingredients"].apply(ast.literal_eval)
 data["directions"] = data["directions"].apply(ast.literal_eval)
 data["NER"] = data["NER"].apply(ast.literal_eval)
@@ -32,16 +17,4 @@ data["ingredients"] = data["ingredients"].apply(lambda x: '\n'.join(x))
 data["directions"] = data["directions"].apply(lambda x: '\n'.join(x))
 data["NER"] = data["NER"].apply(lambda x: ', '.join(x))
 
-
-
-# main(f'{data["title"][0]} \n {data["ingredients"][0]}' )
-
 main(data, nrows)
-#Check for Source
-# source_data = pd.read_csv("recipe catalog raw/recipes_data.csv", nrows=2000000)
-# source_data = source_data["source"]
-# i = 0
-# while i < len(source_data):
-#     if source_data[i] != "Gathered":
-#         print(source_data[i])
-#     i += 1
